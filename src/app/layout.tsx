2628f7b2f338d6db54b82ef60cb5fe9d7f15fe0f
abyan dimas <abyandimasrymussyafa@gmail.com>
2026-01-19 00:39:53 +0700

    add some apps

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,6 +43,9 @@ export const metadata: Metadata = {
   },
 };
 
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -54,6 +57,8 @@ export default function RootLayout({
         <Providers>
           <Header />
           {children}
+          <Analytics />
+          <SpeedInsights />
         </Providers>
       </body>
     </html>
